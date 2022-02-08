a14878c65ebbeb29727fa6ebd81a2097ce8855a7
zhangli <zhangli@flashhold.com>
2022-02-08 17:18:17 +0800

    测试

--- egg/config/config.default.js
+++ egg/config/config.default.js
@@ -17,6 +17,21 @@ module.exports = appInfo => {
     { room: lockpointRoom, name: 'lockPointService' },
     { room: robotRoom, name: 'robotService' },
   ];
+
+  // 关闭 csrf
+  /* config.security = {
+    csrf: {
+      enable: false,
+    },
+  }; */
+
+  // cors 跨域配置
+  /* config.cors = {
+    origin: '*',
+    // 允许请求的方法
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
+  }; */
+
 	config.io = {
 		init: { }, // passed to engine.io
 		namespace: {
@@ -24,6 +39,10 @@ module.exports = appInfo => {
 			  connectionMiddleware: ['connection'], //中间件
 			  packetMiddleware: [],//中间件
 			},
+      /* '/control': {
+			  connectionMiddleware: ['connection'], //中间件
+			  packetMiddleware: [],//中间件
+			}, */
 			'/catch': {},
 		},
 	}
